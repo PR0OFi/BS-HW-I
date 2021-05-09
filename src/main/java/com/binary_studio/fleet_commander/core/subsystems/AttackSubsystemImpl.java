6248e1527987f02f85ea96c3dedf054f6f8a6177
Andrii <59233588+PR0OFi@users.noreply.github.com>
2021-05-09 20:49:17 +0300

    third test from AttakSubsystemTest completed

--- src/main/java/com/binary_studio/fleet_commander/core/subsystems/AttackSubsystemImpl.java
+++ src/main/java/com/binary_studio/fleet_commander/core/subsystems/AttackSubsystemImpl.java
@@ -6,59 +6,70 @@ import com.binary_studio.fleet_commander.core.subsystems.contract.AttackSubsyste
 
 public final class AttackSubsystemImpl implements AttackSubsystem {
 
-	private String name;
+    private String name;
 
-	private PositiveInteger baseDamage;
+    private PositiveInteger baseDamage;
 
-	private PositiveInteger optimalSize;
+    private PositiveInteger optimalSize;
 
-	private PositiveInteger optimalSpeed;
+    private PositiveInteger optimalSpeed;
 
-	private PositiveInteger capacitorUsage;
+    private PositiveInteger capacitorUsage;
 
-	private PositiveInteger pgRequirement;
+    private PositiveInteger pgRequirement;
 
-	public AttackSubsystemImpl(AttackSubsystemBuilder builder){
-		this.name = builder.getName();
-		this.baseDamage = builder.getBaseDamage();
-		this.optimalSize = builder.getOptimalSize();
-		this.optimalSpeed = builder.getOptimalSpeed();
-		this.capacitorUsage = builder.getCapacitorUsage();
-		this.pgRequirement = builder.getPgRequirement();
-	}
+    public AttackSubsystemImpl(AttackSubsystemBuilder builder) {
+        this.name = builder.getName();
+        this.baseDamage = builder.getBaseDamage();
+        this.optimalSize = builder.getOptimalSize();
+        this.optimalSpeed = builder.getOptimalSpeed();
+        this.capacitorUsage = builder.getCapacitorUsage();
+        this.pgRequirement = builder.getPgRequirement();
+    }
 
-	public static AttackSubsystemImpl construct(String name, PositiveInteger powergridRequirments,
-			PositiveInteger capacitorConsumption, PositiveInteger optimalSpeed, PositiveInteger optimalSize,
-			PositiveInteger baseDamage) throws IllegalArgumentException {
+    public static AttackSubsystemImpl construct(String name,
+                                                PositiveInteger powergridRequirments, PositiveInteger capacitorConsumption,
+                                                PositiveInteger optimalSpeed, PositiveInteger optimalSize, PositiveInteger baseDamage) throws IllegalArgumentException {
 
-		if (name.trim().isEmpty()) {
-            throw new IllegalArgumentException("Name should be not null and not empty");}
-		return AttackSubsystemBuilder.named(name).pg(powergridRequirments.value())
-				.capacitorUsage(capacitorConsumption.value()).optimalSpeed(optimalSpeed.value()).
-						optimalSize(optimalSize.value()).damage(baseDamage.value()).construct();
-	}
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name should be not null and not empty");
+        }
+        return AttackSubsystemBuilder.named(name).pg(powergridRequirments.value())
+                .capacitorUsage(capacitorConsumption.value()).optimalSpeed(optimalSpeed.value()).
+                        optimalSize(optimalSize.value()).damage(baseDamage.value()).construct();
+    }
 
-	@Override
-	public PositiveInteger getPowerGridConsumption() {
+    @Override
+    public PositiveInteger getPowerGridConsumption() {
+        return pgRequirement;
+    }
 
-		return pgRequirement;
-	}
+    @Override
+    public PositiveInteger getCapacitorConsumption() {
+        return capacitorUsage;
+    }
 
-	@Override
-	public PositiveInteger getCapacitorConsumption() {
-		return capacitorUsage;
-	}
+    @Override
+    public PositiveInteger attack(Attackable target) {
+        final PositiveInteger targetSize = target.getSize();
+        final PositiveInteger targetSpeed = target.getCurrentSpeed();
+        final String targetName = target.getName();
+        int sizeReductionModifier = 0;
+        int speedReductionModifier = 0;
+        if (targetSize.value() >= optimalSize.value()) {
+            sizeReductionModifier = 1;
+        } else sizeReductionModifier = targetSize.value() / optimalSize.value();
+        if (targetSpeed.value() <= optimalSize.value()) {
+            speedReductionModifier = 1;
+        } else speedReductionModifier = ((2 * targetSpeed.value()) / optimalSpeed.value());
+        final int damage = baseDamage.value() * Math.min(sizeReductionModifier, speedReductionModifier);
 
-	@Override
-	public PositiveInteger attack(Attackable target) {
-		// TODO: Ваш код здесь :)
-		return null;
-	}
+        return new PositiveInteger(damage);
+    }
 
-	@Override
-	public String getName() {
-
-		return name;
-	}
+    @Override
+    public String getName() {
+        return name;
+    }
 
 }
